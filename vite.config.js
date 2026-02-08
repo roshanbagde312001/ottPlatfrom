66fd2fb875b6a4b573095743e1fcd23ed47e2b0a
roshanbagde312001 <jitcse.roshanbagde@gmail.com>
2026-02-08 21:48:13 +0530

    pushed the new dist1

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ottPlatfrom/", // ✅ IMPORTANT FIX
+
   plugins: [react()],
+  
   resolve: {
     alias: {
       '@': '/src',
     },
   },
+
   server: {
     port: 3000,
     open: true
   }
 })
-
